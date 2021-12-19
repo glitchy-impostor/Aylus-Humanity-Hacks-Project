e0f993a80137fa42615dca2ee04f61e94b7ca658
glitchy-impostor <75087610+glitchy-impostor@users.noreply.github.com>
2021-12-19 22:34:05 +0530

    Add files via upload

--- src/components/events/currentEvents.js
+++ src/components/events/currentEvents.js
@@ -12,6 +12,7 @@ const CurrentEvents = () => {
 			data: {'city_name': city},
 			success: function(res){
 				data = res.data;
+				console.log(data);
 			}
 		})
 	}
@@ -22,44 +23,43 @@ const CurrentEvents = () => {
 					<h2>Events</h2>
 					<h4>Activities Going On</h4>
 					<div className='wrapper'>
-            {data.map(d => {
-				      return (
-                <Card 
-                  title={d.name}
-                  description={d.description} 
-                  date={d.date}
-                  creator={d.organiser}
-                  location={d.location}
-                  btn="Sign Up"/>
-              )
-            })}
-	   					
-						{/* <Card 
-							title='Tree Plantr' 
-							description='A tree planting meetup' 
+	    					{data.map(dp => (
+	    						<Card
+							title={dp.name}
+							description={dp.description}
+							date={dp.date}
+							time={dp.time}
+							creator={dp.organiser}
+							location={dp.location}
+							btn="Sign Up"/>
+	    					))}
+	   					/*
+						<Card
+							title='Tree Plantr'
+							description='A tree planting meetup'
 							date="11-25-2022"
 							creator='Save the Trees'
 							location='Yosemite National Park, California'
 							btn="Sign Up"/>
-						<Card 
-							title='Ocean Cleanup' 
-							description='An ocean cleanup meetup' 
+						<Card
+							title='Ocean Cleanup'
+							description='An ocean cleanup meetup'
 							date='12-19-2021'
 							creator='Kenneth Choi'
 							location='New Delhi, India'
 							btn="Sign Up"/>
-						<Card 
-							title='Tree Plantr' 
-							description='A tree planting meetup' 
+						<Card
+							title='Tree Plantr'
+							description='A tree planting meetup'
 							date="1-5-2022"
 							creator='Shannon Lee'
 							location='Seattle, Washington'
-							btn="Sign Up"/> */}
+							btn="Sign Up"/>*/
 					</div>
 				</div>
 			</div>
-       
+
     );
 }
 
-export default CurrentEvents; 
+export default CurrentEvents;
