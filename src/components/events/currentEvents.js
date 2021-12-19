66d41e5b6a5f7591045661721b14f197f77150ea
glitchy-impostor <75087610+glitchy-impostor@users.noreply.github.com>
2021-12-19 18:54:09 +0530

    Update currentEvents.js

--- src/components/events/currentEvents.js
+++ src/components/events/currentEvents.js
@@ -2,12 +2,36 @@ import React from "react";
 import Card from "../cards/cards";
 import "./currentEvents.scss";
 const CurrentEvents = () => {
+	let data = [];
+	function getEvents(){
+		const jq = window.$;
+		let city = 'San Francisco, USA';
+		jq.ajax({
+			url: '/api/search/events',
+			type: 'POST',
+			data: {'city_name': city},
+			success: function(res){
+				data = res.data;
+			}
+		})
+	}
+	getEvents();
     return (
 			<div className='spacer'>
 				<div className='current-events'>
 					<h2>Events</h2>
 					<h4>Activities Going On</h4>
 					<div className='wrapper'>
+	    					{data.map(d => (
+	    						<Card 
+							title=d.name 
+							description=d.description 
+							date=d.date
+							creator=d.organiser
+							location=d.location
+							btn="Sign Up"/>
+	    					))}
+	   					/*
 						<Card 
 							title='Tree Plantr' 
 							description='A tree planting meetup' 
@@ -28,7 +52,7 @@ const CurrentEvents = () => {
 							date="1-5-2022"
 							creator='Shannon Lee'
 							location='Seattle, Washington'
-							btn="Sign Up"/>
+							btn="Sign Up"/>*/
 					</div>
 				</div>
 			</div>
