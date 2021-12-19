9d483f279da02f45b265d5a8753bcf02bb024872
glitchy-impostor <75087610+glitchy-impostor@users.noreply.github.com>
2021-12-19 18:37:57 +0530

    Update addEvent.js

--- src/components/addEvent.js
+++ src/components/addEvent.js
@@ -1,36 +1,78 @@
 import React from "react";
 
 const AddEvents = () => {
+    const onSubmitHandler = (e) => {
+          e.preventDefault();
+          const jq = window.$;
+          let random_key = '';
+          let doa = '';
+          let cookieData = decodeURIComponent(document.cookie).split('; ');
+          let m;
+          for(m in cookieData){
+            if(cookieData[m].includes('random_key=')){
+              random_key = cookieData[m].replace('random_key=', '');
+            }else if(cookieData[m].includes('doa')){
+              doa = cookieData[m].replace('doa=', '');
+            }
+          }
+          let name = document.getElementById('name').value;
+          let description = document.getElementById('description').value;
+          let date = document.getElementById('date').value;
+          let time = document.getElementById('time').value;
+          let location = document.getElementById('location').value;
+          if(random_key != '' && doa != ''){
+            jq.ajax({
+              url: '/api/create/even',
+              type: 'POST',
+              data: {'random_key': random_key, 'doa': doa, 'name':name, 'description': description, 'date':date, 'time':time, 'location':location},
+              success: function(res){
+                if(res.conf == 0){
+                  alert('Event has successfully been Created!');
+                  window.location.href="/home";
+                }else{
+                  alert('You are not logged in! Please Log in to create Events!');
+                }
+              }
+            })
+          }else{
+            alert('You are not logged in! Please Log in to create Events!');
+          }
+    }
     return (
         <div className="outer">
         <div className="inner">
-            <form>
+            <form onSubmit={onSubmitHandler}>
 
                 <h3>Add Event!</h3>
 
                 <div className="form-group">
                     <label>Event Name</label>
-                    <input type="name" className="form-control" placeholder="Enter Event Name" />
+                    <input id="name" type="name" className="form-control" placeholder="Enter Event Name" />
                 </div>
-
+                /*
                 <div className="form-group">
                     <label>Creator</label>
-                    <input type="name" className="form-control" placeholder="Enter name of the event creator or organizer!" />
+                    <input  type="name" className="form-control" placeholder="Enter name of the event creator or organizer!" />
                 </div>
-
+                */
                 <div className="form-group">
                     <label>Location</label>
-                    <input type="name" className="form-control" placeholder="Enter location of event!"/>
+                    <input id="location" type="name" className="form-control" placeholder="Enter general location of event! (General Area Will Do)"/>
                 </div>
 
                 <div className="form-group">
                     <label>Date</label>
-                    <input type="text" className="form-control" placeholder="Enter Date of Event" />
+                    <input id="date" type="text" className="form-control" placeholder="Enter Date of Event" />
+                </div>
+        
+                <div className="form-group">
+                    <label>Time</label>
+                    <input id="type" type="text" className="form-control" placeholder="Enter Time of Event in 24 Hour Format" />
                 </div>
 
                 <div className="form-group">
                     <label>Event Description</label>
-                    <input type="text" className="form-control" placeholder="Enter a description of the event" />
+                    <input id="description" type="text" className="form-control" placeholder="Enter a description of the event! (Also include a way to contact you here)" />
                 </div>
 
                     <button type="submit" className="btn btn-dark btn-lg btn-block">Add Event!</button>
